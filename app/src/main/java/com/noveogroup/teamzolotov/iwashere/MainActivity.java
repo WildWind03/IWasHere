3814d8954da910af83e6d22192c3f14ee50746c9
Alexander Chirikhin <usergreat03@gmail.com>
2016-07-27 15:33:53 +0600

    (+) toolbar (+) new color style for toolbar

--- app/src/main/java/com/noveogroup/teamzolotov/iwashere/MainActivity.java
+++ app/src/main/java/com/noveogroup/teamzolotov/iwashere/MainActivity.java
@@ -1,12 +1,32 @@
 package com.noveogroup.teamzolotov.iwashere;
 
+import android.graphics.Color;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+
+public class MainActivity extends BaseActivity {
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        Drawer drawer = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .build();
+
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_main;
     }
 }
