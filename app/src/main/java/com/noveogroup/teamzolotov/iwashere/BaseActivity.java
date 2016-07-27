46e7605063570097f7cc0a6623cb3f3fe5d7b6ef
Alexander Chirikhin <usergreat03@gmail.com>
2016-07-27 11:30:08 +0600

    (+) BaseActivity

---
+++ app/src/main/java/com/noveogroup/teamzolotov/iwashere/BaseActivity.java
@@ -0,0 +1,25 @@
+package com.noveogroup.teamzolotov.iwashere;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayout());
+        ButterKnife.bind(this);
+    }
+
+    protected abstract int getLayout();
+
+    protected void showToast(String message) {
+        if (null != message) {
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- app/src/main/java/com/noveogroup/teamzolotov/iwashere/MainActivity.java
+++ app/src/main/java/com/noveogroup/teamzolotov/iwashere/MainActivity.java
@@ -1,13 +1,12 @@
 package com.noveogroup.teamzolotov.iwashere;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
     }
 }
