d52f92ddf3891e5ebc69d21c82d389c4a50dac2c
Alexander Chirikhin <usergreat03@gmail.com>
2016-07-28 09:54:05 +0600

    (+) possibility to select item in drawer

--- app/src/main/java/com/noveogroup/teamzolotov/iwashere/MainActivity.java
+++ app/src/main/java/com/noveogroup/teamzolotov/iwashere/MainActivity.java
@@ -1,47 +1,57 @@
 package com.noveogroup.teamzolotov.iwashere;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
-import android.widget.TextView;
+import android.view.View;
 
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
-import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+
+import java.util.logging.Logger;
 
 public class MainActivity extends BaseActivity {
+    private final static int MAP_ID = 1;
+    private final static int LIST_REGIONS_ID = 2;
+    private final static int SETTINGS_ID = 3;
+    private final static int HELP_ID = 4;
+
+    private final static Logger logger = Logger.getLogger(MainActivity.class.getName());
+
     @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+    protected void onPostCreate(@Nullable final Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        PrimaryDrawerItem mapDrawerItem = new PrimaryDrawerItem();
+        final PrimaryDrawerItem mapDrawerItem = new PrimaryDrawerItem();
         mapDrawerItem
+                .withIdentifier(MAP_ID)
                 .withName(R.string.map_string)
                 .withIcon(R.drawable.ic_map_black_24dp);
 
         PrimaryDrawerItem listRegionsDrawerItem = new PrimaryDrawerItem();
         listRegionsDrawerItem
+                .withIdentifier(LIST_REGIONS_ID)
                 .withName(R.string.regions_string)
                 .withIcon(R.drawable.ic_list_black_24dp);
 
         SecondaryDrawerItem settingDrawerItem = new SecondaryDrawerItem();
         settingDrawerItem
+                .withIdentifier(SETTINGS_ID)
                 .withName(R.string.settings_string)
                 .withIcon(R.drawable.ic_settings_black_24dp);
 
         SecondaryDrawerItem helpDrawerItem = new SecondaryDrawerItem();
         helpDrawerItem
+                .withIdentifier(HELP_ID)
                 .withName(R.string.help_string)
                 .withIcon(R.drawable.ic_help_black_24dp);
-
 
 
         Drawer drawer = new DrawerBuilder()
@@ -53,6 +63,24 @@ public class MainActivity extends BaseActivity {
                         new DividerDrawerItem(),
                         settingDrawerItem,
                         helpDrawerItem)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        int itemId = (int) drawerItem.getIdentifier();
+
+                        switch (itemId) {
+                            case MAP_ID:
+                                break;
+                            case LIST_REGIONS_ID:
+                                break;
+                            case SETTINGS_ID:
+                                break;
+                            case HELP_ID:
+                                break;
+                        }
+                        return false;
+                    }
+                })
                 .build();
 
     }
@@ -60,5 +88,21 @@ public class MainActivity extends BaseActivity {
     @Override
     protected int getLayout() {
         return R.layout.activity_main;
+    }
+
+    private void onMapItemSelected() {
+
+    }
+
+    private void onListRegionsSelected() {
+
+    }
+
+    private void onSettingItemSelected() {
+
+    }
+
+    private void onHelpItemSelected() {
+
     }
 }
