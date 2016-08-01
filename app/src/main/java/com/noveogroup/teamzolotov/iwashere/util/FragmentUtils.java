3a2dd82d139685ecf42296807a362de0c5d3f431
Alexander Chirikhin <usergreat03@gmail.com>
2016-08-01 14:14:04 +0600

    backup and restore items in drawer

--- app/src/main/java/com/noveogroup/teamzolotov/iwashere/util/FragmentUtils.java
+++ app/src/main/java/com/noveogroup/teamzolotov/iwashere/util/FragmentUtils.java
@@ -4,9 +4,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
-/**
- * Created by dserov on 01/08/16.
- */
 public class FragmentUtils {
 
     public static void replaceFragment(Fragment fragment, int resId, FragmentManager manager, String tag) {
