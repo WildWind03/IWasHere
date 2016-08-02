08736840071d5ef8f858538aee6e522e7ca2e3c0
Дмитрий Серов <barbari100@gmail.com>
2016-08-02 16:44:57 +0600

    merged backup/restore and location

--- app/src/main/java/com/noveogroup/teamzolotov/iwashere/fragment/ColourMapFragment.java
+++ app/src/main/java/com/noveogroup/teamzolotov/iwashere/fragment/ColourMapFragment.java
@@ -1,7 +1,11 @@
 package com.noveogroup.teamzolotov.iwashere.fragment;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.res.ResourcesCompat;
 import android.util.Log;
 
@@ -18,6 +22,7 @@ import com.google.maps.android.geojson.GeoJsonLayer;
 import com.google.maps.android.geojson.GeoJsonPolygonStyle;
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.noveogroup.teamzolotov.iwashere.R;
+import com.noveogroup.teamzolotov.iwashere.activity.MainActivity;
 import com.noveogroup.teamzolotov.iwashere.database.RegionOrmLiteOpenHelper;
 import com.noveogroup.teamzolotov.iwashere.model.Region;
 
@@ -35,6 +40,7 @@ import rx.schedulers.Schedulers;
 public class ColourMapFragment extends SupportMapFragment implements OnMapReadyCallback {
 
     private static final String TAG = ColourMapFragment.class.getSimpleName();
+    public final static String LOCATION_KEY = "LOCATION_KEY";
 
     private RegionOrmLiteOpenHelper openHelper;
 
@@ -48,13 +54,29 @@ public class ColourMapFragment extends SupportMapFragment implements OnMapReadyC
         if (openHelper == null) {
             openHelper = OpenHelperManager.getHelper(getContext(), RegionOrmLiteOpenHelper.class);
         }
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                getActivity().getPreferences(0).getBoolean(LOCATION_KEY, true)) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+        }
 
         setRetainInstance(true);
         getMapAsync(this);
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        boolean granted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+        getActivity().getPreferences(0).edit().putBoolean(LOCATION_KEY, granted).commit();
+    }
+
+    @Override
     public void onMapReady(final GoogleMap googleMap) {
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                getActivity().getPreferences(0).getBoolean(LOCATION_KEY, true)) {
+            googleMap.setMyLocationEnabled(true);
+        }
 
         googleMap.setIndoorEnabled(false);
         googleMap.setBuildingsEnabled(false);
@@ -101,24 +123,24 @@ public class ColourMapFragment extends SupportMapFragment implements OnMapReadyC
                 }
             }
         })
-            .subscribeOn(Schedulers.computation())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(new Subscriber<GeoJsonLayer>() {
-                @Override
-                public void onCompleted() {
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<GeoJsonLayer>() {
+                    @Override
+                    public void onCompleted() {
 
-                }
+                    }
 
-                @Override
-                public void onError(Throwable e) {
-                    Log.d(TAG, "Error while parsing geojson/regions db", e);
-                }
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "Error while parsing geojson/regions db", e);
+                    }
 
-                @Override
-                public void onNext(GeoJsonLayer geoJsonLayer) {
-                    geoJsonLayer.addLayerToMap();
-                }
-            });
+                    @Override
+                    public void onNext(GeoJsonLayer geoJsonLayer) {
+                        geoJsonLayer.addLayerToMap();
+                    }
+                });
 
     }
 
